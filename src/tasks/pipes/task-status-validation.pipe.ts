b344f0489c74036f29a706863b86c7d9cb3d64b8
Ricardo Borges <ricardo.borges@somosphi.com>
2020-08-08 19:24:50 -0300

    validate task routes

---
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -0,0 +1,20 @@
+import { PipeTransform, BadRequestException } from "@nestjs/common";
+import { TaskStatus } from "../task.model";
+
+export class TaskStatusValidationPipe implements PipeTransform {
+  readonly allowedStatuses = [
+    TaskStatus.OPEN,
+    TaskStatus.IN_PROGRESS,
+    TaskStatus.DONE,
+  ]
+
+  transform(value: any) {
+    value = value.toUpperCase()
+
+    if (!Object.keys(TaskStatus).includes(value)) {
+      throw new BadRequestException(`"${value} is a invalid status"`)
+    }
+
+    return value
+  }
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import * as uuid from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -28,7 +28,11 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find(task => task.id === id)
+    const found = this.tasks.find(task => task.id === id)
+    if (!found) {
+      throw new NotFoundException(`Task with id ${id} not found`)
+    }
+    return found
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
@@ -46,7 +50,8 @@ export class TasksService {
   }
 
   deleteTask(id: string): void {
-    this.tasks = this.tasks.filter(task => task.id !== id)
+    const found = this.getTaskById(id)
+    this.tasks = this.tasks.filter(task => task.id !== found.id)
   }
 
   updateTaskStatus(id: string, status: TaskStatus): Task {
